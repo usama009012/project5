362c54710adfbd1a62c00093644831c47ff4c73c
usama009012 <you@example.com>
2020-10-28 00:41:31 -0700

    heading

--- src/App.js
+++ src/App.js
@@ -1,23 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import {Formik,Form,Field,ErrorMessage } from "formik";
+
+const initialValues = {
+  fname: "",
+  lname: "",
+  age: "",
+};
+const onSubmit = (values) => {
+  alert(JSON.stringify(values,null,2));
+};
+
+const validation = (values) => {
+  let errors = {};
+  if (!values.fname) {
+    errors.fname = "required";
+  }
+  if (!values.lname) {
+    errors.lname = "required";
+  }
+  if (!values.age) {
+    errors.age = "required";
+  }
+  return errors;
+};
 
 function App() {
+
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Formik  
+      initialValues={initialValues}
+      onSubmit={onSubmit}
+      validate={validation}>
+      <Form >
+        <Field
+          type="text"
+          name="fname"
+          id="fname"
+          placeholder="Fname"
+    
+        />
+       <ErrorMessage name="fname"></ErrorMessage>
+        <Field
+          type="text"
+          name="lname"
+          id="lname"
+          placeholder="Lname"
+        
+        />
+       <ErrorMessage name="lname"></ErrorMessage>
+        <Field
+          type="text"
+          name="age"
+          id="age"
+          placeholder="Age"
+         
+        />
+        <ErrorMessage name="age"></ErrorMessage>
+        <button>submit</button>
+      </Form>
+      </Formik>
     </div>
   );
 }
